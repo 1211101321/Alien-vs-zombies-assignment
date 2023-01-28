71d47ad889d06cfd67e4579a8cf6eb0260b3f6eb
yayabae <1211101925@student.mmu.edu.my>
2023-01-28 22:56:46 +0800

    change main

--- Starter-Kit-master/main.cpp
+++ Starter-Kit-master/main.cpp
@@ -1,11 +1,11 @@
 // *********************************************************
 // Course: TCP1101 PROGRAMMING FUNDAMENTALS
 // Year: Trimester 1, 2022/23 (T2215)
-// Lab: TxxL
-// Names: MUHAMMAD ZAFRAN BIN MOHD ANUAR | AHMAD DANIAL BIN AHMAD FAUZI | yaya you can put your name here
-// IDs: 1211101321 | 1211100824 | id also
-// Emails: 1211101321@student.mmu.edu.my | 1211100824@student.mmu.edu.my | MEMBER_EMAIL_3
-// Phones: 0183834854 | 0178714669 | MEMBER_PHONE_3
+// Lab: TT11L | TT11L | TT9L
+// Names: MUHAMMAD ZAFRAN BIN MOHD ANUAR | AHMAD DANIAL BIN AHMAD FAUZI | NUR ALYA NABILAH BINTI MD.NASER
+// IDs: 1211101321 | 1211100824 | 1211101925
+// Emails: 1211101321@student.mmu.edu.my | 1211100824@student.mmu.edu.my | 1211101925@student.mmu.edu.my
+// Phones: 0183834854 | 0178714669 | 0167859895
 // *********************************************************
 
 #include "pf/helper.h"
