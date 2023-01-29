51b515b9c408a92ccf7b30b14ed4c09a24b6f88d
Ahmad Danial <123755014+1211100824@users.noreply.github.com>
2023-01-29 21:01:27 +0800

    Update main.cpp

--- Starter-Kit-master/main.cpp
+++ Starter-Kit-master/main.cpp
@@ -12,6 +12,20 @@
 #include <iostream>
 using namespace std;
 
+void helpCommand()
+{
+    cout << "Commands" << endl;
+    cout << "1. up    - Move up." << endl;
+    cout << "2. down  - Move down." << endl;
+    cout << "3. left  - Move left." << endl;
+    cout << "4. right - Move right." << endl;
+    cout << "5. arrow - Change the direction of an arrow." << endl;
+    cout << "6. help  - Display these user commands." << endl;
+    cout << "7. save  - Save the game." << endl;
+    cout << "8. load  - Load a game." << endl;
+    cout << "9. quit  - Quit the game." << endl;
+}
+
 int main()
 {
     cout << "Assignment (Part 1)" << endl;
