529f2a3c49a347294b1e46a2731942cf8871781e
yayabae <1211101925@student.mmu.edu.my>
2023-01-29 15:53:13 +0800

    alien seperate list

--- Starter-Kit-master/board.cpp
+++ Starter-Kit-master/board.cpp
@@ -41,61 +41,7 @@ public:
     void display() const;
 };
 
-class Alien
-{
 
-private:
-    int dimX_, dimY_, x, y, direction, centerX, centerY;
-    // direction facing (0-up, 1 -right, 2-down, 3-left)
-
-public:
-    Alien(int dimX, int dimY, vector<vector<char>> &map_)
-    {
-        dimX_ = dimX;
-        dimY_ = dimY;
-
-        int centerX = dimX_ / 2; // code that will place the Alien in the center of the board
-        int centerY = dimY_ / 2;
-        if (dimX_ % 2 == 0)
-            centerX = centerX - 1;
-        if (dimY_ % 2 == 0)
-            centerY = centerY - 1;
-
-        if (centerX >= dimX_ || centerY >= dimY_)
-            return;
-        map_[centerY][centerX] = 'A';
-    }
-    // position on the board
-    int direction; // direction facing (0-up, 1-right, 2-down, 3-left)
-    bool operator==(const char &c)
-    {
-        return c == 'A';
-    }
-    void moveUp()
-    {
-        y--; // move one step up
-    }
-    void moveDown()
-    {
-        y++; // move one step down
-    }
-    void moveLeft()
-    {
-        x--; // move left
-    }
-    void moveRight()
-    {
-        x++; // move right
-    }
-    void turnLeft()
-    {
-        direction = (direction + 3) % 4; // turn left
-    }
-    void turnRight()
-    {
-        direction = (direction + 1) % 4; // turn right
-    }
-};
 
 Board::Board()
 {
@@ -226,72 +172,5 @@ int main()
     board.display();
     return 0;
 
-    while (true)
-    {
-        string userInput;
-        std::cout << "command > ";
-        std::cin >> userInput;
-        if (userInput == "help")
-        {
-            std::cout << "\nList of commands: " << '\n'
-                      << std::endl;
-            std::cout << "1. up - move up." << std::endl;
-            std::cout << "2. down - move down." << std::endl;
-            std::cout << "3. left - move left." << std::endl;
-            std::cout << "4. right - move right." << std::endl;
-            std::cout << "5. arrow - change the direction of an arrow." << std::endl;
-            std::cout << "6. help - display the user commands." << std::endl;
-            std::cout << "7. save - save the game." << std::endl;
-            std::cout << "8. load - load a game." << std::endl;
-            std::cout << "9. quit - quit the game." << std::endl;
-        }
-        else
-        {
-            if (userInput == "up")
-            {
-            }
-
-            else if (userInput == "down")
-            {
-            }
-        }
-    }
-    return 0;
-
-    Alien alien;
-    char c = 'A';
-    string input;
-    while (true)
-    {
-        getline(cin, input);
-        for (char c : input)
-        {
-            if (c == 'up')
-            {
-                alien.moveUp();
-            }
-            else if (c == 'down')
-            {
-                alien.moveDown();
-            }
-            else if (c == 'left')
-            {
-                alien.moveLeft();
-            }
-            else if (c == 'right')
-            {
-                alien.moveRight();
-            }
-        }
-
-        for (int i = 0; i < dimY; i++)
-        {
-            for (int j = 0; j < dimX; j++)
-            {
-                cout << Board::map_[i][j] = '|';
-            }
-        }
-        
-        Board_[Y][X] = 'A';
-    }
+   
 }
